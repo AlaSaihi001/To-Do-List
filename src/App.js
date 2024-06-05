acbd7b3db28440648758532cfc9f951da741a32d
AlaSaihi001 <alawork001@gmail.com>
2024-06-05 14:06:54 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-/*import './App.css';
+import './App.css';
 
 function App() {
   return (
@@ -10,29 +9,3 @@ function App() {
 }
 
 export default App;
-*/
-import { useState } from 'react';
-
-export default function MyApp() {
-  return (
-    <div>
-      <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
-    </div>
-  );
-}
-
-function MyButton() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
-  return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
-  );
-}
